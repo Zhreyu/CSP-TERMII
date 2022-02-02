50b41c5fc5687dc904e5591af9a8c5a3029eef24
Shreyas S <96978606+Zhreyu@users.noreply.github.com>
2022-02-02 14:12:59 +0530

    Update ques4.sql

--- ques4.sql
+++ ques4.sql
@@ -1,31 +1,5 @@
-/* question 4 */
-/* creating table */
-CREATE DATABASE TERM2
-USE TERM2
-CREATE TABLE Book
-      ( Book_Id CHAR(10),
-         Book_Name CHAR(15),
-         Author_Name CHAR(17),
-         Publishers CHAR(12),
-         Price DECIMAL,
-         Typee CHAR(10),
-         Quantity INTEGER
-       );
- INSERT INTO Book VALUES ('C0001','Fast Cook','Lata Kapoor','EPB',355.00,'Cookery',5);
- INSERT INTO Book VALUES ('F0001','The tears','Willian Hopkins','First pub',650.00,'Fiction',20);
- INSERT INTO Book VALUES ('T0001','My First C++','Brian & Broke','EPB',350.00,'Text',10);
- INSERT INTO Book VALUES ('T0002','C++ Brain works','A.W. Rossaine','TDH',350.00,'Text',15);
- INSERT INTO Book VALUES ('F0002','Thunderbolts','Anna Roberts','First pub',350.00,'Fiction',50);
-
-CREATE TABLE Issued
-          ( Book_Id CHAR(10),
-            Issuedto CHAR(10),
-            Quantity_Issued INTEGER
-          );
-INSERT INTO Issued VALUES ('T0001','Kamal',4)
-INSERT INTO Issued VALUES ('C0001','Arvind',5)
-INSERT INTO Issued VALUES ('F0001','Suresh',2)
-/*
+/* Shreyas XIIB 52 2021-2022*/
+/*            |QUESTIONS|
 1. To show Book id, Book name, Author name and price of books of First Pub Publisher
 2. To display the names and price of books in ascending order of their prices.
 3. Display the price of book which has price between 300 to 500.
@@ -34,8 +8,15 @@ INSERT INTO Issued VALUES ('F0001','Suresh',2)
 6. To display the Bookname and Price of Books for all books having ‘C++’ in the bookname
 7. Delete the book whose book id is T0002
 8. Show all book whose book name started with character ‘T’ 
-*/
-/* 1st answer
+Give the output of the following
+9. Select Count(*) from Book;
+10. Select Max(Price) from Book where Quantity >=15;
+11. Select Book_Name, Author_Name from Book where Publisher=’First Pub’;
+12. Select count(distinct Publisher) from Book where Price>=400;
+13. Select count(distinct(Publisher)) from book; */
+
+/*        |ANSWERS|                     */
+/* 1st 
 SELECT Book_Id,Book_Name,Author_name,Price
 FROM Book
 WHERE Publishers = 'First pub' */
@@ -69,25 +50,21 @@ SELECT Book_Name,Price
 FROM Book 
 WHERE Book_Name LIKE '%C++%'
 */
-
 /* 7th
 DELETE FROM Book
 WHERE Book_ID = 'T0002'
 
 */
-
 /* 8th
 SELECT *
 FROM Book
 WHERE Book_Name LIKE 'T%'
 */
-
 /*
 9.)5
 10.)650
 11.)Thunderbolts	Anna Roberts and 
     The tears	Willian Hopkins
-
 12.)1
 13.)4
 */
